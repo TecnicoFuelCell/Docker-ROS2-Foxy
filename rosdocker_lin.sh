1b3bc0d7862a092a3837f391b205c0286a6b7333
Diogo Valdrez <diogovaldrez02@gmail.com>
2024-11-14 19:04:05 +0000

    fix: changed the script in order to easally use with TFC repos

--- rosdocker_lin.sh
+++ rosdocker_lin.sh
@@ -3,18 +3,19 @@
 # Prompt the user for image and container names
 read -p "Enter the name of your Docker image: " image_name
 read -p "Enter the name you want to give to the Docker container: " container_name
+read -p "Enter the path to the ros2_ws folder: " ros2_ws_path
 
 # Set the working directory to where the script is located
-scriptPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+#scriptPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Create the directory structure in the same directory as the Dockerfile
-directory="$scriptPath/share/ros2_ws/src"
-if [ ! -d "$directory" ]; then
-    mkdir -p "$directory"
-    echo "Created directory: $directory"
-else
-    echo "Directory already exists: $directory"
-fi
+directory="$ros2_ws_path/src"
+# if [ ! -d "$directory" ]; then
+#     mkdir -p "$directory"
+#     echo "Created directory: $directory"
+# else
+#     echo "Directory already exists: $directory"
+# fi
 
 # Build the Docker image
 echo "Building the Docker image '$image_name'..."
@@ -30,7 +31,7 @@ if [ $? -eq 0 ]; then
     docker run --name "$container_name" \
     --privileged \
     --device=/dev/video0:/dev/video0 \
-    -it -v "$scriptPath/share:/opt/share" \
+    -it -v "$ros2_ws_path:/opt/share/ros2_ws" \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
